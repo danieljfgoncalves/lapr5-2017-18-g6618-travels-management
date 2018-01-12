9bf7f5bf4989685558edeadf45881aafb92ff1f0
Daniel Gonçalves <1151452@isep.ipp.pt>
2018-01-12 15:19:41 +0000

    quick fix ga.pl

--- baseknowledges/ga.pl
+++ baseknowledges/ga.pl
@@ -495,6 +495,15 @@ km_per_hour_to_meter_per_minute(KmH, MMin) :-
 mod(Num1, Num2, NumR) :-
     NumR is Num1-Num1 div Num2*Num2.
 
+factorial(Num, Result) :-
+    factorial2(Num, Result), !.
+factorial2(0, 1).
+factorial2(N, F) :-
+    N>0,
+    N1 is N-1,
+    factorial(N1, F1),
+    F is N*F1.
+
 % ---- DATA STRUCTURES FUNCTION ---- %
 sublist(L, M, N, S) :-
     sublist2(1, L, M, N, S).
